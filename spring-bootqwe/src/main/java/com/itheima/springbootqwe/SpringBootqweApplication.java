34243792e09cb432cd701985edc22c2ecc54c0c3
逐虹君 <2608477279@qq.com>
2026-01-25 21:01:21 +0800

    Initial commit: 初始化项目

---
+++ spring-bootqwe/src/main/java/com/itheima/springbootqwe/SpringBootqweApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootqwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootqweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootqweApplication.class, args);
+	}
+
+}
---
+++ spring-bootqwe/src/test/java/com/itheima/springbootqwe/SpringBootqweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootqwe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootqweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
